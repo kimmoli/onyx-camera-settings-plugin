5b3df739a568408eac19c01b5fade9f282769227
BirdZhang <0312birdzhang@gmail.com>
2018-04-20 16:35:57 +0800

    add zh_CN translation

---
+++ settings/i18n/camera-settings_zh.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS language="zh_CN" version="2.1">
+<context>
+    <name></name>
+    <message id="camera-settings-title">
+        <source>Camera settings</source>
+        <extracomment>page header</extracomment>
+        <translation>相机设置</translation>
+    </message>
+    <message id="primary-camera-settings">
+        <source>Rear camera</source>
+        <extracomment>section header for primary camera settings</extracomment>
+        <translation>后置摄像头</translation>
+    </message>
+    <message id="image-resolution">
+        <source>Image resolution</source>
+        <extracomment>Combobox label for image resolution</extracomment>
+        <translation>图像分辨率</translation>
+    </message>
+    <message id="video-resolution">
+        <source>Video resolution</source>
+        <extracomment>Combobox label for video resolution</extracomment>
+        <translation>视频分辨率</translation>
+    </message>
+    <message id="secondary-camera-settings">
+        <source>Front camera</source>
+        <extracomment>section header for secondary camera settings</extracomment>
+        <translation>前置摄像头</translation>
+    </message>
+    <message id="other-settings-header">
+        <source>Other settings</source>
+        <extracomment>section header for other settings</extracomment>
+        <translation>其他设置</translation>
+    </message>
+    <message id="video-bit-rate-slider-label">
+        <source>Video Bitrate</source>
+        <extracomment>Slider label for video bit rate setting</extracomment>
+        <translation>视频比特率</translation>
+    </message>
+</context>
+</TS>
